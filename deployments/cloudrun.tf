a6b8381f18dae797ae5fd225c390b21eda981f31
Daniel Lorch <dlorch@gmail.com>
2020-07-11 12:36:08 +0200

    Update Cloud Run deployment when underlying :latest image changes (thanks @c2thorn)

--- deployments/cloudrun.tf
+++ deployments/cloudrun.tf
@@ -6,9 +6,7 @@ resource "google_cloud_run_service" "errors-fail-service" {
   template {
     spec {
       containers {
-        // the ":latest" tag is very important, otherwise Terraform will not
-        // re-deploy the Cloud Run service even if the image changes
-        image = "gcr.io/dlorch-bd021/errors-fail:latest"
+        image = data.google_container_registry_image.errors_fail_latest.image_url
         env {
           name  = "PROJECT_ID"
           value = "dlorch-bd021"
@@ -48,4 +46,30 @@ resource "google_cloud_run_domain_mapping" "errors-fail-domain-mapping" {
   spec {
     route_name = google_cloud_run_service.errors-fail-service.name
   }
+}
+
+// in order to update the Cloud Run deployment when the underlying :latest
+// image changes, we will retrieve the sha256_digest of the image through
+// the docker provider, since the google container registry does not seem
+// to have an API. Thanks @c2thorn for the pointers
+// https://github.com/terraform-providers/terraform-provider-google/issues/6706#issuecomment-652009984
+// https://github.com/terraform-providers/terraform-provider-google/issues/6635#issuecomment-647858867
+data "google_client_config" "default" {}
+
+provider "docker" {
+  registry_auth {
+    address  = "gcr.io"
+    username = "oauth2accesstoken"
+    password = data.google_client_config.default.access_token
+  }
+}
+
+data "docker_registry_image" "errors_fail_image" {
+  name = "gcr.io/dlorch-bd021/errors-fail"
+}
+
+data "google_container_registry_image" "errors_fail_latest" {
+  name    = "errors-fail"
+  project = "dlorch-bd021"
+  digest  = data.docker_registry_image.errors_fail_image.sha256_digest
 }
