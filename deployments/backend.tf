4b3367de03e7ea1b4b9521cc13685b729f8a3a00
Daniel Lorch <dlorch@gmail.com>
2021-01-02 23:14:10 +0200

    Pin terraform version

--- deployments/backend.tf
+++ deployments/backend.tf
@@ -3,4 +3,19 @@ terraform {
     bucket = "errors-fail-terraform-state"
     prefix = "errors.fail"
   }
+  required_version = "~> 0.13.5"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.51.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0.0"
+    }
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.9.0"
+    }
+  }
 }
